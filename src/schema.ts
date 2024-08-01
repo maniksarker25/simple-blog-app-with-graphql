2153a048d41e40bc9eaf2d08ad238eb8d16381d0
Manik <tonysins43@gmail.com>
2024-08-01 12:00:35 +0600

    create schema for user , profile and post for both prisma and graphql

---
+++ src/schema.ts
@@ -0,0 +1,33 @@
+export const typeDefs = `#graphql
+
+
+    type Query {
+    me:User
+    posts:[Post]
+    }
+
+  type Post {
+  id:ID!
+  title:String!
+  content:String!
+  author:User!
+  published:Boolean!
+  createdAT:String!
+
+  }
+
+  type User{
+  id:ID!
+  name:String!
+  email:String!
+  createdAt:String!
+  posts:[Post]
+  }
+
+  type Profile{
+  id:ID!
+  bio:String!
+  createdAt:String!
+  user:User!
+  }
+`;
