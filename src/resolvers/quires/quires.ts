d9a4c1324cc3a590613f090d63090b8cb9f0033b
Manik <tonysins43@gmail.com>
2024-08-02 15:41:12 +0600

    get all published post

--- src/resolvers/quires/quires.ts
+++ src/resolvers/quires/quires.ts
@@ -2,4 +2,7 @@ export const Query = {
   users: async (parent: any, args: any, { prisma }: any) => {
     return await prisma.user.findMany();
   },
+  posts: async (parent: any, args: any, { prisma }: any) => {
+    return await prisma.post.findMany({ where: { published: true } });
+  },
 };
