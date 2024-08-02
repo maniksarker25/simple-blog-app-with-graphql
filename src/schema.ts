132c5c853c173bc554f81741b5be590e7794b8d8
Manik <tonysins43@gmail.com>
2024-08-02 11:23:10 +0600

    create schema for add post

--- src/schema.ts
+++ src/schema.ts
@@ -16,35 +16,44 @@ export const typeDefs = `#graphql
     ):AuthPayload,
 
     signin(email:String!, password:String!):AuthPayload
+
+    addPost(title:String!,content:String!): PostResponse
     }
+
+ 
+
+    type Post {
+    id:ID!
+    title:String!
+    content:String!
+    author:User!
+    published:Boolean!
+    createdAT:String!
+    }
+
+    type User{
+    id:ID!
+    name:String!
+    email:String!
+    createdAt:String!
+    posts:[Post]
+    }
+
+    type Profile{
+    id:ID!
+    bio:String!
+    createdAt:String!
+    user:User!
+   }
+
 
     type AuthPayload {
     userError:String
     token:String
     }
 
-  type Post {
-  id:ID!
-  title:String!
-  content:String!
-  author:User!
-  published:Boolean!
-  createdAT:String!
-
-  }
-
-  type User{
-  id:ID!
-  name:String!
-  email:String!
-  createdAt:String!
-  posts:[Post]
-  }
-
-  type Profile{
-  id:ID!
-  bio:String!
-  createdAt:String!
-  user:User!
-  }
+    type PostResponse{
+    postError:String,
+    post:Post
+    }
 `;
